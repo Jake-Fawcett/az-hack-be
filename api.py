7e69d1945facc4d1f85a6d1cd717460941cafc51
JakeF <jakefawcett2298@hotmail.co.uk>
2020-11-18 22:29:54 +0000

    moved some code

--- api.py
+++ api.py
@@ -4,9 +4,6 @@ import mysql.connector
 import csv
 import os
 
-app = Flask(__name__)
-CORS(app)
-
 try:
     # Heroku, use Config vars
     host = os.getenv('host')
@@ -66,4 +63,7 @@ def leaderboard_organisation():
     return ""
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app = Flask(__name__)
+    CORS(app)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
