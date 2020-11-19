f6d876eb2d0c8c1fb7f2602cc54d758a1924dc81
JakeF <jakefawcett2298@hotmail.co.uk>
2020-11-19 09:35:35 +0000

    check env vars

--- api.py
+++ api.py
@@ -93,4 +93,5 @@ def leaderboard_organisation():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
+    print(os.environ)
     app.run(debug=True, host='0.0.0.0', port=port)
