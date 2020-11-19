4106591cf0e0428a88e791b2be24de9581c62c8f
Tom Elvidge <tee504@york.ac.uk>
2020-11-19 10:37:47 +0000

    Implemented create new user (POST)

--- api.py
+++ api.py
@@ -32,11 +32,50 @@ db = mysql.connector.connect(
 app = Flask(__name__)
 CORS(app)
 
+user_table_headers = ["user_id", "user_name", "diet_default", "car_travel_default", "train_travel_default",
+        "bus_travel_default", "food_disposal_default", "plastic_disposal_default", "paper_disposal_default",
+        "glass_disposal_default", "tin_disposal_default", "mobile_screentime_default", "computer_screetime_default",
+        "tv_screentime_default"]
+
 @app.route("/users/<string:user_id>/", methods=["GET", "PUT", "POST", "DELETE"])
 def users(user_id):
     if request.method == "POST":
-        #request.form
-        return ""
+        user = request.json
+        print(user)
+        print(type(user))
+        # Make sure the user doesn't already exist before creatin one.
+        get_user_query = "SELECT * FROM Users WHERE user_id = '{}';"
+        cursor = db.cursor(dictionary=True)
+        cursor.execute(get_user_query.format(user_id))
+        results = cursor.fetchall()
+
+        if len(results) == 0:
+            # No user with user_id so create new user.
+            # Extract the user features from the request body.
+            user_features = []
+            for header in user_table_headers:
+                if type(user[header]) == str:
+                    # Wrap strings with quotes
+                    user_features.append("'{}'".format(user[header]))
+                else:
+                    user_features.append(str(user[header]))
+            # Insert the new user.
+            insert_user_query = "INSERT INTO Users ({}) VALUES ({});".format(", ".join(user_table_headers), ", ".join(user_features))
+            # print(insert_user_query)
+            cursor.execute(insert_user_query)
+
+            # Insert the user into Organisations.
+            for organisation_name in user["organisations"]:
+                insert_organisations_query = "INSERT INTO Organisations (user_id, organisation_name) VALUES ('{}', '{}');".format(user_id, organisation_name)
+                print(insert_organisations_query)
+                cursor.execute(insert_organisations_query)
+
+            # Commit changes to mysql.
+            db.commit()
+            return "Done."
+        else: # len(results) == 1
+            return "User already exists."
+
     elif request.method == "GET":
         # Get the user with user_id.
         get_user_query = "SELECT * FROM Users WHERE user_id = '{}';"
@@ -60,6 +99,7 @@ def users(user_id):
         # return the user.
         return user
     elif request.method == "PUT":
+        # Not implemented as updating the user in frontend uses post.
         return ""
     else: # request.method == "DELETE":
         # Query to delete from users.
